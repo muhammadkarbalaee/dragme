a7a5a5582fa63fe623d2b33806887dded851a077
Muhammad Karbalaee <77221151+muhammadksht@users.noreply.github.com>
2022-12-11 23:14:33 +0330

    web added

--- lib/API/load_images.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:math';
-
-String getImageURL() {
-  int index = Random().nextInt(200) + 1;
-  return "https://raw.githubusercontent.com/muhammadkarbalaee/ball-images/master/balls/$index.png";
-}
--- lib/widgets/swipe_direction.dart
+++
@@ -1,6 +0,0 @@
-enum SwipeDirection{
-  left,
-  right,
-  top,
-  bottom
-}
